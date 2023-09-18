45a4fd7631059ad350ddaddb0ecfdabdec190062
gqfrisby <101162719+gqfrisby@users.noreply.github.com>
2023-09-17 21:01:36 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿namespace Lab2ServSide
+{
+    internal class Program
+    {
+        private static List<VideoGame> games = new List<VideoGame>();
+        static void Main(string[] args)
+        {
+            StreamReader sr = new(@"C:\Users\gqfri\source\repos\Lab2ServSide\Lab2ServSide\videogames.csv");
+            string header = sr.ReadLine();
+            Dictionary<string, List<VideoGame>> PlatformSort = new();
+            HashSet<string> uniquePlatform = new HashSet<string>();
+            string NullCheck;
+            while ((NullCheck = sr.ReadLine()) != null)
+            {
+                games.Add(ProcessCSVLine(NullCheck));
+            }
+            foreach (VideoGame game in games)
+            {
+                if (uniquePlatform.Contains(game.Platform))
+                {}
+                else
+                {
+                    uniquePlatform.Add(game.Platform);
+                }
+            }
+            //Dictionary<string, dynamic> makeTables = new();
+            foreach (string publisher in uniquePlatform)
+            {
+                List<VideoGame> newList = new List<VideoGame>();
+                foreach (VideoGame game in games)
+                {
+                    if(game.Publisher.Equals(publisher))
+                    {
+                        newList.Add(game);
+                    }
+                    PlatformSort[publisher] = newList;
+                }
+            }
+            foreach (VideoGame game in games)
+            {
+                PlatformSort[game.Platform].Add(game);
+            }
+            string userChoice;
+            do
+            {
+                Console.WriteLine("(Stop 6.) Input the publisher you would like to search (Case Sensistive): ");
+                userChoice = Console.ReadLine();
+                if (PlatformSort.ContainsKey(userChoice))
+                {
+                    var top5Games = PlatformSort[userChoice].OrderByDescending(VideoGame => VideoGame.GlobalSales).Take(5);
+                    //Console.WriteLine("Test1");
+                    //Console.WriteLine(PlatformSort["DS"].Count());
+                    foreach (var game in top5Games)
+                    {
+                        //Console.WriteLine("Test3");
+                        Console.WriteLine(game.ToString());
+                    }
+                }
+            } while (userChoice != "6");
+
+        }
+
+        static VideoGame ProcessCSVLine(string LineFromCSV)
+        {
+            string[] rawGameParts = LineFromCSV.Split(",");
+            List<string> cleanGameParts = new List<string>();
+            string cleanString = string.Empty;
+            for (int i = 0; i < rawGameParts.Length; i++)
+            {
+                cleanString += rawGameParts[i];
+                // more data processing / Cleaning?
+                cleanGameParts.Add(cleanString);
+                cleanString = "";
+            }
+            string Name = cleanGameParts[0];
+            string Platform = cleanGameParts[1];
+            string preparse1 = cleanGameParts[2];
+            int Year = Int32.Parse(preparse1);
+            string Genre = cleanGameParts[3];
+            string publisher = cleanGameParts[4];
+            string preParse2 = cleanGameParts[5];
+            double NASales = double.Parse(preParse2);
+            string preParse3 = cleanGameParts[6];
+            double EUSales = double.Parse(preParse3);
+            string preParse4 = cleanGameParts[7];
+            double JPSales = double.Parse(preParse4);
+            string preParse5 = cleanGameParts[8];
+            double OtherSales = double.Parse(preParse5);
+            string preparse6 = cleanGameParts[9];
+            double GlobalSales = double.Parse(preparse6);
+            VideoGame cleanGame = new VideoGame(Name, Platform, Year, Genre, publisher, NASales, EUSales, JPSales, OtherSales, GlobalSales);
+            return cleanGame;
+        }
+    }
+}
---
+++ VideoGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2ServSide
+{
+    internal class VideoGame : IComparable<VideoGame>
+    {
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public double NASales { get; set; }
+        public double EUSales { get; set; }
+        public double JPSales { get; set; }
+        public double OtherSales { get; set; }
+        public double GlobalSales { get; set; }
+
+        public VideoGame(string name, string platform, int year, string genre, string publisher, double nASales, double eUSales, double jPSales, double otherSales, double globalSales)
+        {
+            Name = name;
+            Platform = platform;
+            Year = year;
+            Genre = genre;
+            Publisher = publisher;
+            NASales = nASales;
+            EUSales = eUSales;
+            JPSales = jPSales;
+            OtherSales = otherSales;
+            GlobalSales = globalSales;
+        }
+
+        public override string ToString()
+        {
+            string info = "";
+            info = (this.Name + ", " + this.Platform + ", " + this.Year + ", " + this.Genre + ", " + this.Publisher + ", " + this.NASales + ", " + this.EUSales + ", " + this.JPSales + ", " + this.OtherSales + ", " + this.GlobalSales);
+            return info;
+        }
+
+        public int CompareTo(VideoGame? other)
+        {
+            return string.Compare(this.Name, other.Name);
+        }
+    }
+}
